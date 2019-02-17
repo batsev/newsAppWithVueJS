6fbe2510bdbc61ad86f785aede0a11eed29f0d3a
Nikita Batsev <nikita.batsev@gmail.com>
2019-02-17 19:52:16 +0300

    v-card added

--- src/components/Headline.vue
+++ src/components/Headline.vue
@@ -2,8 +2,10 @@
   <v-container grid-list-md text-xs-center>
     <v-layout row wrap>
       <v-flex class="px-5" :key="headline.id" v-for="headline in headlines" md4 sm6 lg4>
-        <v-img height="300" :src="headline.urlToImage"></v-img>
-        <v-card-text class="px-0">{{headline.title}}</v-card-text>
+        <v-card elevation="0">
+          <v-img height="300" :src="headline.urlToImage"></v-img>
+          <v-card-text>{{headline.title}}</v-card-text>
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
